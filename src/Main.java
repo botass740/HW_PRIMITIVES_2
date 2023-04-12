d488336dccaebb8cfb008f3f81b6375dcaf9f269
Max test <botass740@gmail.com>
2023-04-12 22:00:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 300;
+        int cash = 1100;
+        int bonus = cash / 100;
+
+        if (cash > 1001) {
+            bonus = cash / 100;
+        } else {
+            bonus = 0;
+    }
+        int balans = balance + cash + bonus;
+        System.out.println("Начислено бонусов - " + bonus);
+        System.out.println("Ваш баланс равен " + balans);
+    }
+}
