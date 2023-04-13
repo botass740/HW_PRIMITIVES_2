4b4d1a165fc407a86e4b955ad39a50d7041a64c8
Max test <botass740@gmail.com>
2023-04-13 20:42:40 +0300

    change Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 300;
-        int cash = 1100;
+        int cash = 1001;
         int bonus = cash / 100;
 
-        if (cash > 1001) {
+        if (cash > 1000) {
             bonus = cash / 100;
         } else {
             bonus = 0;
-    }
+        }
         int balans = balance + cash + bonus;
         System.out.println("Начислено бонусов - " + bonus);
         System.out.println("Ваш баланс равен " + balans);
